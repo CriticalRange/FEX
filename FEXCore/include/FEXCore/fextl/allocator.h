f35dfe486ccb141a2a4700c1c87a956aee701cb6
CriticalRange <muhammedkosgen0608112@gmail.com>
2026-03-11 03:10:19 +0300

    fextl: make FEXAlloc satisfy allocator rebind/equality requirements
    Remove std::allocator inheritance from FEXAlloc and add an explicit
    rebind definition. Also template operator== to support comparisons
    across rebound allocator types, matching allocator requirements.

--- FEXCore/include/FEXCore/fextl/allocator.h
+++ FEXCore/include/FEXCore/fextl/allocator.h
@@ -9,10 +9,12 @@ namespace fextl {
  * @brief C++ allocator class interface in to FEXCore::Allocator for memory allocations.
  */
 template<typename T>
-class FEXAlloc : public std::allocator<T> {
+class FEXAlloc { // VEXA_FIXES [allocator.requirements] states that rebinding an allocator to the same type should result in the original allocator. So we add rebind and make operator== templated instead.
 public:
   using value_type = T;
   using propagate_on_container_move_assignment = std::true_type;
+  template<class U>
+  struct rebind { using other = FEXAlloc<U>; };
 
   FEXAlloc() noexcept {}
   template<class U>
@@ -26,7 +28,8 @@ public:
     ::FEXCore::Allocator::aligned_free(p);
   }
 
-  inline bool operator==(const FEXAlloc&) const {
+  template<class U>
+  inline bool operator==(const FEXAlloc<U>&) const {
     return true;
   }
 };
